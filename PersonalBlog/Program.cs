92ae3e00f1d13938b5f3927d3d99c2e7802380f1
Emadram <refemad@outlook.com>
2025-05-17 17:02:19 +0300

    AdminPanelUpdate-FixPatch#1

--- PersonalBlog/Program.cs
+++ PersonalBlog/Program.cs
@@ -63,6 +63,9 @@ using (var scope = app.Services.CreateScope())
 {
     var context = scope.ServiceProvider.GetRequiredService<ApplicationDbContext>();
     context.Database.EnsureCreated();
+    
+    // Seed settings
+    await DataSeeder.SeedSettingsAsync(context);
 }
 
 app.Run();
